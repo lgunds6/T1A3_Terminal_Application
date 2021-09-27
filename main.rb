bdc95e580f8d72dce0d6483902fcc5aad8edda61
lgunds6 <lgunds6@gmail.com>
2021-09-27 15:53:45 +1000

    Added code to capture the users name, goal, and user details. I have also added in another page activity_level.rb for later use

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 
 puts "Welcome to Luke's fitness app"
 
-class Goal
+class UserGoal
     attr_reader :name
     attr_reader :art
     attr_reader :options
@@ -10,22 +10,34 @@ class Goal
     def initialize()
 
         print "Enter your first name: "
+       
 
         @name = gets.strip
+        
 
-        @intro = "Please tell us your fitness goal from the following three options"
+        @intro = "Please choose your fitness goal from the following three options"
 
         @options = []
         @options.push("Would you like to lose weight?")
-        @options.push("Would you like tomaitain your current weight?")
-        @options.push("Would you like tobulk up?")
+        @options.push("Would you like to maintain your current weight?")
+        @options.push("Would you like to bulk up?")
     end
 
     def selecting_your_goal ()
-        puts "#{"
+        puts "#{@name}, #{@intro}"
+    
+        options_index = 0
+        for option in @options
+            puts "#{options_index + 1}. #{option}"
+            options_index += 1
+        end
+        print "Choose your goal"
+        user_choice = gets.to_i - 1
+    end
 
 end
-
+welcome = UserGoal.new
+welcome.selecting_your_goal()
 
 class UserDetails
     attr_reader :name
@@ -36,8 +48,6 @@ class UserDetails
 
 
 def initialize ()
-    print "Please enter your first name"
-    @name = gets.chomp
     print "Please enter your age"
     @age = gets.strip
     print "Please enter your weight"
@@ -49,4 +59,15 @@ def initialize ()
 end
 end
 details = UserDetails.new
+class activityLevel
+    attr_reader :activity_level
+    
+    print "#{@name}, Please select your daily activity level from the following options"
+
+    @activity_level []
+    @activity_level.push("Would you like to lose weight?")
+    @activity_level.push("Would you like to maintain your current weight?")
+    @activity_level.push("Would you like to bulk up?")
+
+
 
