9cc4cece735e8a4758821b551ded6805eca9e176
lgunds6 <lgunds6@gmail.com>
2021-09-30 20:07:19 +1000

    added artii gem to app

--- main.rb
+++ main.rb
@@ -1,10 +1,13 @@
 require 'colorize'
 require 'colorized_string'
+require 'artii'
 require_relative "activity_level"
 require_relative "user_details"
 require_relative "user"
 require_relative "calorie"
 
+Artii::Base.new :font => 'slant'
+artii ('Art!')
 puts "Welcome to Luke's fitness app".colorize(:blue)
 
 
