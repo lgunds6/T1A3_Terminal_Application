d46c6a6e5bb4c874b8f5d9872ffec8e81fb56076
lgunds6 <lgunds6@gmail.com>
2021-10-01 09:14:07 +1000

    Added in a workout to print for user

--- workout.rb
+++ workout.rb
@@ -1,3 +1,29 @@
-puts "Whould like like to read the help guide?"
-if yes 
-    print 
+lass Workout
+    attr_reader :workout
+    
+    def initialize()
+  
+    @workout = []
+    @workout.push("5 sets of Dumbell Chest Press - 10-12 reps".colorize(:yellow))
+    @workout.push("5 sets of Barbell Squats - 10-12 reps".colorize(:yellow))
+    @workout.push("5 sets of Chin Ups - 10-12 reps".colorize(:yellow))
+    @workout.push("5 sets of Ab Crunches - 15-20 reps".colorize(:yellow))
+
+
+    end
+    def your_workout ()
+  
+        puts "Would you like a custom made workout? "
+       if answer = "y" 
+        workout_index = 0
+        for activity in @workout
+            puts "#{workout_index + 1}. #{workout}"
+            workout_index += 1
+        end
+    else answer = "n"
+        puts "Thanks for using LGFIT"
+    
+    end
+
+end
+end 
